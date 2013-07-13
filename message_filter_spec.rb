8f414b3108ca8d44c9f757bd7d2588d823ab6750
vagrant <vagrant@rails-starter-box>
2013-07-13 03:02:37 +0000

    detect?メソッド作成

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,4 +1,10 @@
-require "spec"
+require "rubygems"
+require "rspec"
+require "./message_filter"
 
-describe MessageFileter do
+describe MessageFilter do
+  it "should detect message with NG word" do
+    filter = MessageFilter.new("foo")
+    filter.detect?("hello from foo").should == true
+  end
 end
