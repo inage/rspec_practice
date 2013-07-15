1d943b0fc49146cb8e1e9e99f88184b5ead23662
vagrant <vagrant@rails-starter-box>
2013-07-15 21:42:13 +0000

    describeのネスト

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,21 +2,23 @@ require "rubygems"
 require "rspec"
 require "./message_filter"
 
-share_examples_for "MessageFilter with argument 'foo'" do
+describe MessageFilter do
+  share_examples_for "MessageFilter with argument 'foo'" do
 
-  it { should be_detect("hello from foo") }
-  it { should_not be_detect("hello, world!") }
-end
+    it { should be_detect("hello from foo") }
+    it { should_not be_detect("hello, world!") }
+  end
 
-describe MessageFilter, "with argument 'foo'" do
+  describe MessageFilter, "with argument 'foo'" do
 
-  subject { MessageFilter.new("foo") }
-  it_should_behave_like "MessageFilter with argument 'foo'"
-end
+    subject { MessageFilter.new("foo") }
+    it_should_behave_like "MessageFilter with argument 'foo'"
+  end
 
-describe MessageFilter, "with arument 'foo','bar'" do
+  describe MessageFilter, "with arument 'foo','bar'" do
 
-  subject { MessageFilter.new('foo','bar') }
-  it { should be_detect("hello from bar") }
-  it_should_behave_like "MessageFilter with argument 'foo'"
+    subject { MessageFilter.new('foo','bar') }
+    it { should be_detect("hello from bar") }
+    it_should_behave_like "MessageFilter with argument 'foo'"
+  end
 end
