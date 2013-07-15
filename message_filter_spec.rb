4707157b4e0c4971078fa6dd6b2064fe940e669d
vagrant <vagrant@rails-starter-box>
2013-07-15 21:43:53 +0000

    状況を説明するときはcontextを使おう

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -9,13 +9,13 @@ describe MessageFilter do
     it { should_not be_detect("hello, world!") }
   end
 
-  describe MessageFilter, "with argument 'foo'" do
+  context MessageFilter, "with argument 'foo'" do
 
     subject { MessageFilter.new("foo") }
     it_should_behave_like "MessageFilter with argument 'foo'"
   end
 
-  describe MessageFilter, "with arument 'foo','bar'" do
+  context MessageFilter, "with arument 'foo','bar'" do
 
     subject { MessageFilter.new('foo','bar') }
     it { should be_detect("hello from bar") }
